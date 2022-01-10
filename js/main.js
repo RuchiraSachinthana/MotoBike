fb343591761f4a3222bea15d4502a595c8b842e1
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2022-01-10 22:04:51 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 var c = document.createElement("canvas");
 var ctx = c.getContext("2d");
-c.width = 500;
+c .width = 500;
 c.height = 350;
 document.body.appendChild(c);
 
 var perm = [];
 while (perm.length < 255){
     while(perm.includes(val = Math.floor(Math.random()*255)));
+    perm.push(val);
 }
 
+var lerp = (a, b, t) => a + (b-a)*t ;
 var noise = x=>{
-    return
+    return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math(x));
 }
 
 function loop(){
-    ctx.fillstyle = "#19f";
+    ctx.fillStyle = "#19f";
     ctx.fillRect(0,0,c.width, c.height);
 
     requestAnimationFrame(loop);
